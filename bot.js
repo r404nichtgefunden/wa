fe2c56f99ee28fd910de0ba49b43742e48241710
r404nichtgefunden <yulixixi7@gmail.com>
2025-05-24 17:30:05 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const {
   useMultiFileAuthState,
   fetchLatestBaileysVersion,
   makeInMemoryStore
-} = require('baileys')
+} = require('@whiskeysockets/baileys')
 const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
